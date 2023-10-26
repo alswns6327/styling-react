5604342cbd5013498aefad30930786294092e5a2
alswns6327 <alswns6327@naver.com>
2023-10-27 00:58:02 +0900

    classnames with CSSModule

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import styles from './CSSModule.module.css';
 
+const cx = classNames.bind(styles);
+
 const CSSModule = () => {
+    console.log(cx('wrapper', {a :false}, 'inverted'));
+    console.log(classNames('wrapper', 'inverted'));
     console.log([styles.inverted, styles.wrapper].join(' '));
     return (
         <>
             <div className={`${styles.inverted} ${styles.wrapper}`}>
+                hi i'm <span className='something'>CSS Module</span>
+            </div>
+            <div className={[styles.inverted, styles.wrapper].join(' ')}>
+                hi i'm <span className='something'>CSS Module</span>
+            </div>
+            <div className={styles.wrapper}>
+                hi i'm <span className='something'>CSS Module</span>
+            </div>
+            <div className={cx('wrapper', 'inverted')}>
                 hi i'm <span className='something'>CSS Module</span>
             </div>
             <div className={[styles.inverted, styles.wrapper].join(' ')}>
